a2b37c158de9e68cc829d8c4fcb8db1523a04d64
Thomas <thomas.hansson85@gmail.com>
2017-03-16 14:20:25 +0100

    Added auction details

---
+++ app/auction/auction-details.controller.js
@@ -0,0 +1,15 @@
+angular.module("auction").controller("auctionDetailsController", ["$scope", "$routeParams", "$location", "auctionService",
+    function ($scope, $routeParams, $location, auctionService) {
+
+        auctionService.getAuctionByID($routeParams.auctionId).then(function (response) {
+            $scope.auction = response.data;
+        }, function (errorResponse) {
+
+        });
+
+        $scope.auctionClickedInDetails = function () {
+            $location.path("/");
+
+        };
+
+    }]);
--- app/login/login.service.js
+++ app/login/login.service.js
@@ -1,6 +1,6 @@
 angular.module("login").factory("loginService",["$http", function ($http) {
     var isLoggedIn = false;
-    var customerIdAfterLogin;
+
 
 
     return {
@@ -13,19 +13,16 @@ angular.module("login").factory("loginService",["$http", function ($http) {
         login: function (user) {
 
             return $http.post("http://nackademiska-api.azurewebsites.net/api/account/login", user).then(function (response) {
-
+                console.log(response.data);
                 isLoggedIn = true;
-                customerIdAfterLogin = response.data.customerId;
+
             })
 
         },
         isLoggedIn: function () {
             return isLoggedIn;
 
-        },
-        customerIdAfterLogin: function () {
-            return customerIdAfterLogin;
-
         }
+
     };
 }]);
