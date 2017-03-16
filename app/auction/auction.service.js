52187df36cebb6bb19490c854016f4af66dcbdcb
SamLindley <samlindley0@gmail.com>
2017-03-16 13:19:43 +0100

    auctions/categories

--- app/auction/auction.service.js
+++ app/auction/auction.service.js
@@ -1,3 +1,14 @@
-/**
- * Created by Thomas on 2017-03-16.
- */
+angular.module("auction").factory("auctionService", ["$http", function ($http) {
+    return {
+        getAuctions: function () {
+            return $http.get("http://nackademiska-api.azurewebsites.net/api/auction");
+        },
+        getAuction: function (id) {
+            return $http.get("http://nackademiska-api.azurewebsites.net/api/auction" + id);
+        },
+        getCategories: function () {
+            return $http.get("http://nackademiska-api.azurewebsites.net/api/category");
+        }
+
+    }
+}]);
