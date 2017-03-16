0940e09eab2c44650ea4ec0c420a06c73c4f63c4
Thomas <thomas.hansson85@gmail.com>
2017-03-16 16:23:40 +0100

    Added auction details

--- app/supplier/supplier.controller.js
+++ app/supplier/supplier.controller.js
@@ -1,6 +1,15 @@
-angular.module("supplier").controller("supplierController", ["$scope","$routeParams", "supplierService",
-    function ($scope, $routeParams, supplierService) {
-        supplierService.getSupplier($routeParams.supplierId).then(function (response) {
+angular.module("supplier").controller("supplierController", ["$scope","$routeParams","$location", "supplierService",
+    function ($scope, $routeParams,$location, supplierService) {
+
+        supplierService.getAllSuppliers().then(function (response) {
+            $scope.suppliers = response.data;
+        });
+
+
+        supplierService.getSupplierById($routeParams.supplierId).then(function (response) {
             $scope.supplier = response.data;
-        })
+            console.log(response.data);
+        }, function (errorResponse) {
+
+    });
 }]);
--- app/supplier/supplier.service.js
+++ app/supplier/supplier.service.js
@@ -1,10 +1,11 @@
 angular.module("supplier").factory("supplierService", ["$http", function ($http) {
     return {
-        getSupplier: function (id) {
-            $http.get("http://nackademiska-api.azurewebsites.net/api/supplier/" + id)
-        },
         getAllSuppliers: function () {
-            $http.get("http://nackademiska-api.azurewebsites.net/api/supplier/")
+            $http.get("http://nackademiska-api.azurewebsites.net/api/supplier")
+        },
+        getSupplierById: function (id) {
+            $http.get("http://nackademiska-api.azurewebsites.net/api/supplier/" + id)
         }
+
     }
 }]);
