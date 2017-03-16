630d4ba3868a9d3bcccf030d6666b8d224381ac2
RUMPNISSENS-ASK\Gustav <gustav_hassler@hotmail.com>
2017-03-16 15:35:06 +0100

    Detail View v1

--- Android/NackademiskaAuction/app/src/main/java/com/example/gustav/androidmenyer/DetailActivity.java
+++ Android/NackademiskaAuction/app/src/main/java/com/example/gustav/androidmenyer/DetailActivity.java
@@ -1,13 +1,21 @@
 package com.example.gustav.androidmenyer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -26,13 +34,22 @@ public class DetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         Auction auction = (Auction) intent.getSerializableExtra(MainActivity.AUCTION);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(auction.getName());
-    }
+        TextView nameView = (TextView) findViewById(R.id.auctionNameViewDetail);
+        TextView priceView = (TextView)  findViewById(R.id.auctionPriceViewDetail);
+        ImageView imageView = (ImageView) findViewById(R.id.auctionImageViewDetail);
 
+        Locale swedish = new Locale("sv", "SWE");
+        NumberFormat priceFormat = NumberFormat.getNumberInstance(swedish);
+        String price = priceFormat.format(auction.getPrice());
+
+        nameView.setText(auction.getName());
+        priceView.setText(price);
+        Picasso.with(this).load(auction.getImageUrl()).into(imageView);
+    }
 }
