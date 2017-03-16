1cf012f5624f503eddd575f35a7b16ee378304d5
Thomas <thomas.hansson85@gmail.com>
2017-03-16 11:21:48 +0100

    Added login

--- app/app.config.js
+++ app/app.config.js
@@ -5,6 +5,10 @@ angular.module("app").config(["$routeProvider", "$locationProvider", function ($
             templateUrl: "app/auction/auction.template.html",
             controller: "auctionController"
         })
+        .when("/login", {
+            templateUrl: "app/login/login.template.html",
+            controller: "loginController"
+        })
 
         .otherwise("/");
     $locationProvider.html5Mode(true);
