1c5f0ca30cf5bf9c2c4726976477be479f7419b2
SamLindley <samlindley0@gmail.com>
2017-03-16 13:19:43 +0100

    auctions/categories

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,1 @@
-angular.module("app", ["ui.bootstrap", "ngRoute", "login"]);
+angular.module("app", ["ui.bootstrap", "ngRoute", "login", "auction"]);
--- app/auction/auction.controller.js
+++ app/auction/auction.controller.js
@@ -1,3 +1,17 @@
-/**
- * Created by Thomas on 2017-03-16.
- */
+angular.module("auction").controller("auctionController", ["$scope", "auctionService",
+    "$location", function ($scope, auctionService, $location) {
+
+    auctionService.getAuctions().then(function (response) {
+        $scope.auctions = response.data;
+    });
+
+    $scope.auctionClicked = function (id) {
+        $location.path("/product/" + id);
+    };
+
+    auctionService.getCategories().then(function (response) {
+        $scope.categories = response.data;
+    })
+
+
+}]);
