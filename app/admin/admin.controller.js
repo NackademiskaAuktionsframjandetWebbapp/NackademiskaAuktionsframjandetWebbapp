08edfcc1cbb61e30bb2a4d011d04827bdda5be84
SamLindley <samlindley0@gmail.com>
2017-03-20 11:46:39 +0100

    admin files

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -2,6 +2,9 @@ angular.module("admin").controller("adminController", ["$q", "$scope", "adminSer
     function ($q, $scope, adminService, auctionService) {
     var finishedAuctions = [];
     var bids = [];
+    var totalsByMonth= [];
+    var totalInMonth;
+    var monthExists = false;
 
     adminService.getFinishedAuctions().then(function (response) {
         finishedAuctions = response.data;
@@ -13,10 +16,26 @@ angular.module("admin").controller("adminController", ["$q", "$scope", "adminSer
            for (var i = 0; i<finishedAuctions.length; i++){
                bids = response[i].data;
                finishedAuctions[i].highestBid = bids[bids.length-1].bidPrice;
+               finishedAuctions[i].monthAndYear = finishedAuctions[i].endTime.substring(0,7),
+               totalInMonth = {monthAndYear: finishedAuctions[i].endTime.substring(0,7), total: finishedAuctions[i].highestBid};
+               console.log(totalInMonth);
+               monthExists = false;
+               for (var i = 0; i<totalsByMonth.length; i++){
+                   if (totalInMonth.monthAndYear == totalsByMonth[i].monthAndYear){
+                       totalsByMonth.total += totalInMonth.total;
+                       monthExists = true;
+
+                   }
+               }
+               if (!monthExists){
+                   totalsByMonth.push(totalInMonth);
+               }
 
            };
 
+
         });
+        console.log(totalsByMonth);
 
 
 
