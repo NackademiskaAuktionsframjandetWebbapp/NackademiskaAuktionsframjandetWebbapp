d8f90d2feb8077479c5240d92fa739f1887550bf
SamLindley <samlindley0@gmail.com>
2017-03-20 15:57:49 +0100

    admin files

--- app/auction/auction.filter.js
+++ app/auction/auction.filter.js
@@ -2,7 +2,8 @@ angular.module("auction").filter("activeAuctions", function () {
     return function (input) {
         var activeAuctions = [];
         angular.forEach(input, function (auction) {
-            if (auction.highestBid >= auction.buyNowPrice && (auction.endTime < new Date() && auction.startTime > new Date())){
+            if (auction.highestBid != auction.buyNowPrice ||
+                (new Date(auction.endTime) <= new Date() && new Date(auction.startTime) >= new Date())){
                 activeAuctions.push(auction);
             }
         });
